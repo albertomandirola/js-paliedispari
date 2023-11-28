c8484eda3b3fc952df548346609f2211f7e09ca9
albertomandirola <albymandi00@gmail.com>
2023-11-28 17:36:15 +0100

    [Fixed] es palindroma and create folder for the es

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -3,20 +3,33 @@
 //Creare una funzione per capire se la parola inserita è palindroma
 
 //creo la funzione
-function palindroma(word){
+function isPalindroma(word){
     //creo all'interno della funzione un if che permettera di verficare se la parola e' palindroma
     /* per fare cio applico diverse funzioni a word che mi permettono di trasformala in un arrey '.split'
     poi di invertire la posizione degli elementi '.reverse', '.join'ricompone la stringa
     e infine applico ad entrambi i membri '.toLowerCase' per ignorare la presenza di maiuscole */
     if(word.toLowerCase() ==  word.split('').reverse().join('').toLowerCase()){
-        console.log(`la parola ${word} e' palindroma`)
+        return true
     }
     else{
-        console.log(`la prola ${word} non e' palindroma`)
+        return false
     }
     
 }
 //dichiaro la variabile parola che mi permettera di chidere all utente la parola da verificare
 let parola = prompt('inserire una parola')
 //applico la funzione con la variabile parola scelta dall'utente
-palindroma(parola)
+if( isPalindroma(parola)){
+    alert('la parola e\' palindroma')
+}
+else{
+    alert('la parola non e\' palindroma')
+}
+
+
+
+
+
+
+
+
